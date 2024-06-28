02f0b383ff7f6638f9a217aff60cf55bcd26fc8b
robotnev <ngveah2@gmail.com>
2024-06-28 14:22:47 -0700

    beginning login/signup

--- index.js
+++ index.js
@@ -33,6 +33,41 @@ app.post("/create", async (req, res) => {
     });
 })
 
+app.get('/', async (req, res) => {
+    const { user, password } = req.query;
+    if (user && password) {
+        // Attempt to login
+        const userRecord = await prisma.user.findUnique({
+            where: { user }
+        });
+        bcrypt.compare(password, userRecord.hashedPassword, function(err, result) {
+            if (result) {
+                res.status(200).json({});
+            } else {
+                res.status(401).json({"error": "Invalid username or password"});
+            }
+        });
+    } else {
+        // Display sign up/login form
+        res.send(`
+            <form action="/create" method="post">
+                <label for="user">Username:</label>
+                <input type="text" id="user" name="user"><br><br>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password"><br><br>
+                <input type="submit" value="Sign Up">
+            </form>
+            <form action="/login" method="post">
+                <label for="user">Username:</label>
+                <input type="text" id="user" name="user"><br><br>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password"><br><br>
+                <input type="submit" value="Login">
+            </form>
+        `);
+    }
+});
+
 
 app.post("/login", async (req, res) => {
     const {user, password} = req.body;
@@ -48,6 +83,11 @@ app.post("/login", async (req, res) => {
         }
     });
 })
+
+app.get('/sample-path', (req, res) => {
+    res.send('This is a sample response!')
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
